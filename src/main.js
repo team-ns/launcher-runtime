da955837104c46297a6e4575edb351a442c78947
RussianJeb <russianjeb@protonmail.com>
2021-01-11 12:02:43 +0300

    feat: import roboto font, launcher communication and i18n

--- src/main.js
+++ src/main.js
@@ -1,16 +1,22 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+import "typeface-roboto";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import * as backend from "./backend";
+import i18n from "./i18n";
 
 Vue.config.productionTip = false;
+
+window.app = { backend };
 
 new Vue({
   router,
   store,
   vuetify,
+  i18n,
   render: h => h(App)
 }).$mount("#app");
