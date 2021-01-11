e2032e66c21e90fa986a12af21739ca43381f3df
RussianJeb <russianjeb@protonmail.com>
2021-01-11 12:02:04 +0300

    feat: add to store launcher settings and utils variables

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,73 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    load: false,
+    login: "",
+    text: "",
+    error: false,
+    ok: false,
+    maxRam: 2048,
+    download: {
+      totalSize: 0,
+      currentSize: 0
+    },
+    settings: {
+      gameDir: "",
+      saveData: false,
+      ram: 1024
+    },
+    profiles: []
+  },
+  mutations: {
+    error: (state, payload) => {
+      state.error = true;
+      state.text = payload;
+    },
+    ok: (state, payload) => {
+      state.ok = true;
+      state.text = payload;
+    },
+    load: (state, payload) => {
+      state.load = true;
+      state.text = payload;
+    },
+    loaded: state => {
+      state.load = false;
+      state.text = "";
+    },
+    close: state => {
+      state.load = false;
+      state.error = false;
+      state.ok = false;
+      state.text = "";
+    },
+    settings: (state, payload) => {
+      state.settings = payload;
+    },
+    profiles: (state, payload) => {
+      state.profiles = payload;
+    }
+  },
+  getters: {
+    downloadStatus: state => {
+      return (state.download.currentSize * 100) / state.download.totalSize;
+    },
+    hasMessage: state => {
+      return state.ok || state.error;
+    },
+    loading: state => {
+      return state.load;
+    },
+    messageColor: state => {
+      if (state.error) {
+        return "red darken-1";
+      } else if (state.ok) {
+        return "green darken-1";
+      }
+      return null;
+    }
+  },
   actions: {},
   modules: {}
 });
