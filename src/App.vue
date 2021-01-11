49b6cfecca726a7b55806b63b620e588a178a78b
RussianJeb <russianjeb@protonmail.com>
2021-01-11 12:00:25 +0300

    feat: update app screen

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,57 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld />
+    <v-dialog v-model="$store.getters.hasMessage" persistent max-width="290">
+      <v-card>
+        <v-card-title
+          >{{ $store.state.error ? $t("dialog.error") : $t("dialog.ok") }}
+        </v-card-title>
+        <v-card-text>{{ $store.state.text }}</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            :color="$store.getters.messageColor"
+            text
+            @click="$store.commit('close')"
+            >{{ $t("dialog.button") }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-main class="background">
+      <router-view v-if="!$store.getters.loading" />
+      <loading v-else />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import Loading from "@/views/Loading";
+import { eventBus } from "@/eventBus";
 
 export default {
   name: "App",
-
-  components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
+  components: { Loading },
+  created() {
+    this.$store.commit("load", this.$t("message.connect"));
+    window.external.invoke(`"ready"`);
+    this.$router.push("/").catch(() => {});
+    eventBus.$on("login", () => {
+      this.$router.push("/menu").catch(() => {});
+    });
+    eventBus.$on("download", () => {
+      this.$router.push("/download").catch(() => {});
+    });
+  }
 };
 </script>
+
+<style>
+body {
+  user-select: none;
+  -ms-overflow-style: none;
+}
+.background {
+  background: url("./assets/background.jpg") center;
+  background-size: cover;
+}
+</style>
