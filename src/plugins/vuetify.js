c85b242afdd4e7a8178e3941e0cc3638d18977c6
RussianJeb <russianjeb@protonmail.com>
2021-01-11 11:53:51 +0300

    feat: add vuetify and vue-i18n to dependency

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#8bc34a",
+        secondary: "#cddc39",
+        accent: "#ffc107",
+        error: "#f44336",
+        warning: "#ff9800",
+        info: "#4caf50",
+        success: "#009688"
+      }
+    }
+  }
+});
