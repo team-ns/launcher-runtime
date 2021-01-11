546702fa0e1e80a04d21e1a502ee6e7c9ce4251b
RussianJeb <russianjeb@protonmail.com>
2021-01-11 11:58:25 +0300

    feat: add download screen

---
+++ src/views/Download.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-layout fluid fill-height align-center justify-center>
+    <v-flex xs12 sm8 md6>
+      <v-container class="justify-center">
+        <v-card>
+          <v-card-title class="justify-center">
+            <h3>{{ $t("message.download") }}</h3>
+          </v-card-title>
+          <v-card-subtitle>
+            <v-layout row justify-center align-center>
+              <v-progress-linear
+                color="primary"
+                height="20"
+                :value="$store.getters.downloadStatus"
+                striped
+              ></v-progress-linear>
+            </v-layout>
+          </v-card-subtitle>
+        </v-card>
+      </v-container>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: "Download"
+};
+</script>
+
+<style scoped></style>
